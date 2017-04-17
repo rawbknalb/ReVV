6c336be58a9ac41caaf83dec91866e8b6d9baf72
rawbknalb <rawbknalb@gmail.com>
2017-04-17 17:18:06 +0200

    Added Tabs to Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,7 +7,12 @@ import { Grid, Col, Row } from "react-styled-flexboxgrid";
 import { Panel, PanelHeadline } from "../style/Panel";
 import { VVLogo } from "../style/Logo";
 import { PortfolioHeadlineInfo } from "../style/PortfolioHeadline";
-import { Hero } from "../style/Hero";
+
+import Tabs, { TabPane } from "rc-tabs";
+import ScrollableInkTabBar from "rc-tabs/lib/ScrollableInkTabBar";
+import TabContent from "rc-tabs/lib/SwipeableTabContent";
+import SwipeableInkTabBar from "rc-tabs/lib/SwipeableInkTabBar";
+import "rc-tabs/assets/index.css";
 
 import MotionMenu from "./MotionMenu";
 
@@ -21,6 +26,8 @@ class Dashboard extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
+
+  callback() {}
 
   render() {
     if (this.props.assetAllocation) {
@@ -36,7 +43,7 @@ class Dashboard extends Component {
             Das dynamische ETF-VestFolio mit Chancen in Aktien-
             und Rohstoffmärkten zu erhöhtem Risiko
           </PortfolioHeadlineInfo>
-          <br/>
+          <br />
           {/* Wrap in Charts Component ????? */}
           <Grid>
             <Row>
@@ -49,8 +56,27 @@ class Dashboard extends Component {
             <Row>
               <Col xs={12} md={12} lg={8}>
                 <Panel>
-                  <PanelHeadline>Performance</PanelHeadline>
-                  <PortfolioHistoryChart />
+                  <Tabs
+                    defaultActiveKey="1"
+                    onChange={this.callback}
+                    renderTabBar={() => (
+                      <SwipeableInkTabBar pageSize={5} speed={10} />
+                    )}
+                    renderTabContent={() => <TabContent />}
+                  >
+                    <TabPane tab="Historie" key="1">
+                      {" "}<PortfolioHistoryChart />
+                    </TabPane>
+                    <TabPane tab="Anlageaufteilung" key="2">
+                      {" "}
+                      <AllocationDonutPieChart
+                        data={this.props.assetAllocation}
+                      />
+                    </TabPane>
+                    <TabPane tab="Prognose" key="3">third</TabPane>
+                    <TabPane tab="Testo" key="4">third</TabPane>
+                    <TabPane tab="Testu" key="5">third</TabPane>
+                  </Tabs>
                 </Panel>
               </Col>
               <Col xs={false} md={12} lg={4}>
