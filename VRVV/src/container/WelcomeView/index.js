ecd1d9774378de79a2ae841fcefd484443e0a799
rawbknalb <rawbknalb@gmail.com>
2017-06-05 22:58:23 +0200

    Added PerformanceChart in € ++ Asset Allocation Chart ++ Animate SelectedPortfolioPanel to Top ++ SelectPortfolio() fetches Charts if Charts exist in Store  before Click

--- VRVV/src/container/WelcomeView/index.js
+++ VRVV/src/container/WelcomeView/index.js
@@ -5,23 +5,14 @@ import { connect } from "react-redux";
 import { WelcomeViewHeadlines } from "../../components/Headlines";
 
 import VideoPanel from "../../components/VideoPanel";
-import CurvedPanel from "../../components/CurvedPanel";
+import CurvedPanel from "../../components/CurvedPanel"
 
 class WelcomeView extends React.Component {
   render() {
     return (
       <View>
         <WelcomeViewHeadlines />
-        <CurvedPanel width={1000} height={400} density={4000}>
-          <VideoPanel
-            videoWebm="Warum_VisualVest_vid.Webm"
-            videoMp4="Warum_VisualVest_vid.mp4"
-          />
-        </CurvedPanel>
-        <VideoPanel
-          videoWebm="Warum_VisualVest_vid.Webm"
-          videoMp4="Warum_VisualVest_vid.mp4"
-        />
+        <VideoPanel videoWebm="Warum_VisualVest_vid.Webm" videoMp4="Warum_VisualVest_vid.mp4"/>
       </View>
     );
   }
